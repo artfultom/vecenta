215169f988370a409b48c041db8100b6f815c862
Andrey Vasilyev <artfultomcat@gmail.com>
2020-02-25 23:32:03 +0300

    [Add] handler

---
+++ src/main/java/my/artfultom/vecenta/transport/MethodHandler.java
@@ -0,0 +1,21 @@
+package my.artfultom.vecenta.transport;
+
+import java.util.function.Function;
+
+public class MethodHandler {
+    private final String name;
+    private final Function<Request, Response> function;
+
+    public MethodHandler(String name, Function<Request, Response> function) {
+        this.name = name;
+        this.function = function;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Response execute(Request in) {
+        return this.function.apply(in);
+    }
+}
